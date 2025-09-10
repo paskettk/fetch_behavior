c29c9b9ad3730b7d9f839b00ffe5ab24f809cb23
paskettk <paskettk@oregonstate.edu>
2025-09-10 12:08:02 -0700

    modified testing scripts

--- scripts/demo_task.py
+++ scripts/demo_task.py
@@ -7,23 +7,25 @@ from fetch_api import FetchAPI
 if __name__ == "__main__":
     robot = FetchAPI()
 
-    # Move base
+    # Move base (example position)
     robot.move_to(1.0, 0.0)
 
-    # Define a "cup" pose
+    # Define "cup" pose
     cup_pose = geometry_msgs.msg.Pose()
     cup_pose.position.x = 0.6
     cup_pose.position.y = 0.0
     cup_pose.position.z = 0.9
     cup_pose.orientation.w = 1.0
 
-    # Pick + place
+    # Pick cup
     robot.pick_object(cup_pose)
 
+    # Define "shelf" pose
     shelf_pose = geometry_msgs.msg.Pose()
     shelf_pose.position.x = 0.8
     shelf_pose.position.y = -0.2
     shelf_pose.position.z = 1.0
     shelf_pose.orientation.w = 1.0
 
+    # Place cup
     robot.place_object(shelf_pose)
--- scripts/fetch_api.py
+++ scripts/fetch_api.py
@@ -13,7 +13,7 @@ class FetchAPI:
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node('fetch_api', anonymous=True)
 
-        # Arm + Gripper groups
+        # Arm + Gripper MoveIt groups
         self.arm = moveit_commander.MoveGroupCommander("arm")
         self.gripper = moveit_commander.MoveGroupCommander("gripper")
 
@@ -21,7 +21,7 @@ class FetchAPI:
         self.nav_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.nav_client.wait_for_server()
 
-    # -------- Navigation --------
+    # ---- Navigation ----
     def move_to(self, x, y, yaw=0.0):
         """Navigate base to (x, y, yaw) in map frame."""
         goal = MoveBaseGoal()
@@ -30,15 +30,14 @@ class FetchAPI:
 
         goal.target_pose.pose.position.x = x
         goal.target_pose.pose.position.y = y
-        goal.target_pose.pose.orientation.w = 1.0  # simple, could convert yaw→quat
+        goal.target_pose.pose.orientation.w = 1.0  # simple, ignoring yaw for now
 
         self.nav_client.send_goal(goal)
         self.nav_client.wait_for_result()
         return self.nav_client.get_result()
 
-    # -------- Arm control --------
+    # ---- Arm ----
     def move_arm_to_pose(self, x, y, z, qw=1.0, qx=0.0, qy=0.0, qz=0.0):
-        """Move arm end-effector to a Cartesian pose."""
         pose_target = geometry_msgs.msg.Pose()
         pose_target.position.x = x
         pose_target.position.y = y
@@ -55,34 +54,35 @@ class FetchAPI:
         return success
 
     def move_arm_joints(self, joint_values):
-        """Directly set arm joint values (list of 7)."""
         self.arm.go(joint_values, wait=True)
         self.arm.stop()
 
-    # -------- Gripper control --------
+    # ---- Gripper ----
     def open_gripper(self):
-        self.gripper.set_joint_value_target([0.04, 0.04])  # open fingers
+        self.gripper.set_joint_value_target([0.04, 0.04])
         self.gripper.go(wait=True)
 
     def close_gripper(self):
-        self.gripper.set_joint_value_target([0.0, 0.0])  # close fingers
+        self.gripper.set_joint_value_target([0.0, 0.0])
         self.gripper.go(wait=True)
 
-    # -------- High-level manipulation --------
+    # ---- High-level ----
     def pick_object(self, pose):
-        """Example: move above object, lower, close gripper."""
-        # move above
+        # Move above
         self.move_arm_to_pose(pose.position.x, pose.position.y, pose.position.z + 0.1)
-        # lower
+        # Lower
         self.move_arm_to_pose(pose.position.x, pose.position.y, pose.position.z)
-        # grab
+        # Grab
         self.close_gripper()
-        # lift
+        # Lift
         self.move_arm_to_pose(pose.position.x, pose.position.y, pose.position.z + 0.1)
 
     def place_object(self, pose):
-        """Move to target, lower, open gripper."""
+        # Move above
         self.move_arm_to_pose(pose.position.x, pose.position.y, pose.position.z + 0.1)
+        # Lower
         self.move_arm_to_pose(pose.position.x, pose.position.y, pose.position.z)
+        # Release
         self.open_gripper()
+        # Lift
         self.move_arm_to_pose(pose.position.x, pose.position.y, pose.position.z + 0.1)
